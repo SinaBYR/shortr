a9eb28d49097dd1e4593025f2785f8bdf595f600
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-03-06 00:11:45 +0330

    Add fetching current user's data route.

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -22,18 +22,19 @@ router.post('/new', async (req, res) => {
   const { username, password } = req.body;
 
   if(!username || !password) {
-    // render a view
+    // 1. render a view
     return res.status(401).json('Authentication failed');
   }
 
   try {
-    // hash password
-    const registerRes = await pool.query(`
+    // 1. hash password
+    // 2. retrieve id column
+    const registerUserRes = await pool.query(`
       insert into user_account (username, password)
       values ($1, $2) returning username;
     `,[username, password]);
 
-    if(!registerRes.rowCount) {
+    if(!registerUserRes.rowCount) {
       throw new Error('An unexpected error occured.');
     }
 
@@ -41,10 +42,35 @@ router.post('/new', async (req, res) => {
       username
     };
 
-    // render a view
+    // 1. render a view
     res.status(201).json('User account created successfully.');
   } catch(err) {
     res.status(500).json(err.message);
+  }
+})
+
+router.get('/me', async (req, res) => {
+  const { user } = req.session;
+  if(!user) {
+    return res.status(401).json('Authentication failed');
+  }
+
+  try {
+    // 1. add other info to the table
+    const fetchUserRes = await pool.query(`
+      select username
+      from user_account
+      where username = $1;
+    `, [user.username]);
+
+    if(!fetchUserRes.rowCount) {
+      return res.status(404).send('User not found.');
+    }
+
+    // 1. render a view
+    res.json(fetchUserRes.rows[0]);
+  } catch(err) {
+    res.status(500).send(err.message); // or "Server Error"
   }
 })
 
