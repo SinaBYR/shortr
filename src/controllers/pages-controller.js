74f7266eee72ff358023a280c00b7ec31a79c64d
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-03-06 13:56:39 +0330

    Register route for login view as well as its session middleware..

--- src/controllers/pages-controller.js
+++ src/controllers/pages-controller.js
@@ -1,3 +1,11 @@
-exports.renderIndexPage = function(req, res) {
+exports.renderIndexPage = function(_req, res) {
   res.render('pages/index.ejs');
+}
+
+exports.renderLoginPage = function(req, res) {
+  if(req.session.user) {
+    return res.render('pages/admin');
+  }
+
+  res.render('pages/login');
 }
--- src/routes/pages.route.js
+++ src/routes/pages.route.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { renderIndexPage } = require('../controllers/pages-controller');
+const { renderIndexPage, renderLoginPage } = require('../controllers/pages-controller');
+const session = require('../middleware/session');
 
 router.get('/', renderIndexPage);
+
+router.get('/login', session(), renderLoginPage);
 
 module.exports = router;
