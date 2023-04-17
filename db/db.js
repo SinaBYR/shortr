7021f3a875b7b17367a289ad42862a17353f13e3
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-04-17 01:41:59 -0400

    Use .env file and dotenv package to load database credentials.

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-  database: 'shortr',
-  host: '127.0.0.1',
-  port: 5432,
-  user: 'postgres',
-  password: 'Sina13801111'
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: +process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD
 });
 
 module.exports = pool;
