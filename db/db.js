1922ede73183fdd2e86b1080f4b9d0afd7e95d61
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2023-04-17 02:07:18 -0400

    Use connectionString to connect to postgres database instead of credentials object.

--- db/db.js
+++ db/db.js
@@ -1,11 +1,8 @@
 const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new pg.Pool({
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  port: +process.env.DATABASE_PORT,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD
+  connectionString
 });
 
 module.exports = pool;
